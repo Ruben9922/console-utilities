64ec8950daf8ea97e5a59b3165294d757febad74
Ruben9922 <Ruben9922@users.noreply.github.com>
2016-08-20 21:35:20 +0100

    Added current project files

---
+++ Ruben9922.Utilities.ConsoleUtilities/ConsoleReadUtilities.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Ruben9922.Utilities.ConsoleUtilities
+{
+    public class ConsoleReadUtilities
+    {
+        public static int ReadInt(string prompt, string errorMessage = "Integers only!", int? min = null, int? max = null)
+        {
+            int result;
+            bool inputValid = false;
+            do
+            {
+                Console.Write(prompt);
+                if (int.TryParse(Console.ReadLine(), out result) && (!min.HasValue || result >= min)
+                    && (!max.HasValue || result < max))
+                {
+                    inputValid = true;
+                }
+                else
+                {
+                    Console.WriteLine(errorMessage);
+                }
+            } while (!inputValid);
+            return result;
+        }
+
+        public static string ReadOptionString(string[] validOptions, string prompt = "Enter option: ")
+        {
+            string option = "";
+            bool inputValid = false;
+            while (!inputValid)
+            {
+                Console.Write(prompt);
+                option = Console.ReadLine().ToLower();
+                foreach (string validOption in validOptions)
+                {
+                    if (option == validOption)
+                    {
+                        inputValid = true;
+                        break;
+                    }
+                }
+                if (!inputValid)
+                {
+                    Console.WriteLine("Invalid option!");
+                }
+            }
+            return option;
+        }
+
+        public static int ReadOptionInt(string[] options, string prompt = "Enter option: ")
+        {
+            int optionCount = options.Length;
+            for (int i = 0; i < optionCount; i++)
+            {
+                Console.WriteLine("  {0}: {1}", i, options[i]);
+            }
+            int option;
+            bool inputValid;
+            do
+            {
+                option = ReadInt(prompt);
+                inputValid = option >= 0 && option < optionCount;
+                if (!inputValid)
+                {
+                    Console.WriteLine("Invalid option!");
+                }
+            } while (!inputValid);
+            return option;
+        }
+    }
+}
